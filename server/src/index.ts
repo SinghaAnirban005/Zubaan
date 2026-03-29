868af006e793fb7fc714b2c8f73cce088ab1fe45
SinghaAnirban005 <anirbansingha20@gmail.com>
2026-03-29 17:31:05 +0530

    chore: register route for subtitles

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Express } from "express"
 import dotenv from "dotenv"
+
 import audioRouter from "./routers/audio.router"
+import subtitleRouter from "./routers/subtitle.router"
+
 import path from 'path'
 
 dotenv.config({
@@ -12,6 +15,7 @@ const app: Express = express()
 app.use(express.json())
 
 app.use('/api/v1/audio', audioRouter)
+app.use('/api/v1/subtitle', subtitleRouter)
 
 app.listen(3000)
 
