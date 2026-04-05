8362d89d83454e2cf24a4199e699838540b2c6ab
SinghaAnirban005 <anirbansingha20@gmail.com>
2026-04-05 17:33:25 +0530

    feat: add /me endpoint

--- server/src/routers/auth.router.ts
+++ server/src/routers/auth.router.ts
@@ -2,10 +2,10 @@ import { Router } from "express"
 import passport from "../lib/passport"
 import { generateToken } from "../utils/jwt"
 import { authMiddleware } from "../middlewares/auth.middleware"
+import { prisma } from "../lib/prisma"
 
 const router: Router = Router()
 
-// Google Login
 router.get(
   "/google",
   passport.authenticate("google", {
@@ -13,7 +13,6 @@ router.get(
   })
 )
 
-// Google Callback
 router.get(
   "/google/callback",
   passport.authenticate("google", {
@@ -37,7 +36,39 @@ router.get(
   }
 )
 
-// Logout
+router.get("/me", authMiddleware, async (req, res) => {
+  try {
+    const userId = (req as any).userId
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        createdAt: true
+      }
+    })
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      })
+    }
+
+    res.status(200).json({
+      message: 'usset fetched',
+      data: user
+    })
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      message: "Internal server error"
+    })
+  }
+})
+
 router.post("/logout", authMiddleware, async (req, res) => {
   res.json({
     message: "Logged out successfully"
