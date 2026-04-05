53ab1cbf3a4aec2fb2b61cf94b6e45add4ed1185
SinghaAnirban005 <anirbansingha20@gmail.com>
2026-04-05 17:32:56 +0530

    fix: mkdir uploads on absence

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -1,5 +1,20 @@
 import multer from "multer"
+import fs from "fs"
+import path from "path"
 
-export const upload = multer({
-    dest: './uploads'
+const uploadDir = path.resolve("uploads")
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true })
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir)
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname)
+    }
 })
+
+export const upload = multer({ storage })
